b8f496bcbd9512241891ac3bc0696ede41d94052
lanbo <15701674082@163.com>
2022-08-15 17:44:23 +0800

    优化进度条

--- assets/Script/Controller/LoginController.js
+++ assets/Script/Controller/LoginController.js
@@ -1,78 +1,48 @@
-// Learn cc.Class:
-//  - [Chinese] http://docs.cocos.com/creator/manual/zh/scripting/class.html
-//  - [English] http://www.cocos2d-x.org/docs/creator/en/scripting/class.html
-// Learn Attribute:
-//  - [Chinese] http://docs.cocos.com/creator/manual/zh/scripting/reference/attributes.html
-//  - [English] http://www.cocos2d-x.org/docs/creator/en/scripting/reference/attributes.html
-// Learn life-cycle callbacks:
-//  - [Chinese] http://docs.cocos.com/creator/manual/zh/scripting/life-cycle-callbacks.html
-//  - [English] http://www.cocos2d-x.org/docs/creator/en/scripting/life-cycle-callbacks.html
-
 import AudioUtils from "../Utils/AudioUtils";
 
 cc.Class({
-    extends: cc.Component,
+  extends: cc.Component,
 
-    properties: {
-        // foo: {
-        //     // ATTRIBUTES:
-        //     default: null,        // The default value will be used only when the component attaching
-        //                           // to a node for the first time
-        //     type: cc.SpriteFrame, // optional, default is typeof default
-        //     serializable: true,   // optional, default is true
-        // },
-        // bar: {
-        //     get () {
-        //         return this._bar;
-        //     },
-        //     set (value) {
-        //         this._bar = value;
-        //     }
-        // },
-        loadingBar: {
-            type: cc.ProgressBar,
-            default: null,
-        },
-        loginButton: {
-            type: cc.Button,
-            default: null,
-        },
-        worldSceneBGM:{
-            type: cc.AudioClip,
-            default: null,
-        }
+  properties: {
+    loadingBar: {
+      type: cc.ProgressBar,
+      default: null,
     },
-
-    // LIFE-CYCLE CALLBACKS:
-
-    onLoad () {
-        this.gameSceneBGMAudioId = cc.audioEngine.play(this.worldSceneBGM, true, 1);
+    loginButton: {
+      type: cc.Button,
+      default: null,
     },
-
-    start () {
-
-    },
-
-    onLogin: function(){
-        this.loadingBar.node.active = true;
-        this.loginButton.node.active = false;
-        this.loadingBar.progress = 0;
-        let backup = cc.loader.onProgress;
-        cc.loader.onProgress = function (count, amount) {
-            this.loadingBar.progress = count / amount;
-        }.bind(this);
-
-        cc.director.preloadScene("Game", function () {
-            cc.loader.onProgress = backup;
-            this.loadingBar.node.active = false;
-            this.loginButton.node.active = true;
-            cc.director.loadScene("Game");
-        }.bind(this));
-    },
-
-    onDestroy: function(){
-        cc.audioEngine.stop(this.gameSceneBGMAudioId);
+    worldSceneBGM: {
+      type: cc.AudioClip,
+      default: null,
     }
+  },
 
-    // update (dt) {},
+  onLoad() {
+    this.gameSceneBGMAudioId = cc.audioEngine.play(this.worldSceneBGM, true, 1);
+  },
+
+  onLogin: function () {
+    this.last = 0;
+    this.loadingBar.node.active = true;
+    this.loginButton.node.active = false;
+    this.loadingBar.progress = 0;
+    this.loadingBar.barSprite.fillRange = 0;
+    cc.loader.onProgress = (count, amount, item) => {
+      let progress = (count / amount).toFixed(2);
+      if (progress > this.loadingBar.barSprite.fillRange) {
+        this.loadingBar.barSprite.fillRange = count / amount;
+      }
+    };
+    cc.director.preloadScene("Game", function () {
+      this.loadingBar.node.active = false;
+      this.loginButton.node.active = false;
+      // cc.log("加载成功");
+      cc.director.loadScene("Game");
+    }.bind(this));
+  },
+
+  onDestroy: function () {
+    cc.audioEngine.stop(this.gameSceneBGMAudioId);
+  }
 });
--- assets/Script/Model/CellModel.js
+++ assets/Script/Model/CellModel.js
@@ -1,102 +1,102 @@
 import { CELL_TYPE, ANITIME, CELL_STATUS, GRID_HEIGHT } from "./ConstValue";
 export default class CellModel {
-    constructor() {
-        this.type = null;
-        this.status = CELL_STATUS.COMMON;
-        this.x = 1;
-        this.y = 1;
-        this.startX = 1;
-        this.startY = 1;
-        this.cmd = [];
-        this.isDeath = false;
-        this.objecCount = Math.floor(Math.random() * 1000);
-    }
+  constructor() {
+    this.type = null;
+    this.status = CELL_STATUS.COMMON;
+    this.x = 1;
+    this.y = 1;
+    this.startX = 1;
+    this.startY = 1;
+    this.cmd = [];
+    this.isDeath = false;
+    this.objecCount = Math.floor(Math.random() * 1000);
+  }
 
-    init(type) {
-        this.type = type;
-    }
+  init(type) {
+    this.type = type;
+  }
 
-    isEmpty() {
-        return this.type == CELL_TYPE.EMPTY;
-    }
+  isEmpty() {
+    return this.type == CELL_TYPE.EMPTY;
+  }
 
-    setEmpty() {
-        this.type = CELL_TYPE.EMPTY;
-    }
-    setXY(x, y) {
-        this.x = x;
-        this.y = y;
-    }
+  setEmpty() {
+    this.type = CELL_TYPE.EMPTY;
+  }
+  setXY(x, y) {
+    this.x = x;
+    this.y = y;
+  }
 
-    setStartXY(x, y) {
-        this.startX = x;
-        this.startY = y;
-    }
+  setStartXY(x, y) {
+    this.startX = x;
+    this.startY = y;
+  }
 
-    setStatus(status) {
-        this.status = status;
-    }
+  setStatus(status) {
+    this.status = status;
+  }
 
-    moveToAndBack(pos) {
-        var srcPos = cc.v2(this.x, this.y);
-        this.cmd.push({
-            action: "moveTo",
-            keepTime: ANITIME.TOUCH_MOVE,
-            playTime: 0,
-            pos: pos
-        });
-        this.cmd.push({
-            action: "moveTo",
-            keepTime: ANITIME.TOUCH_MOVE,
-            playTime: ANITIME.TOUCH_MOVE,
-            pos: srcPos
-        });
-    }
+  moveToAndBack(pos) {
+    var srcPos = cc.v2(this.x, this.y);
+    this.cmd.push({
+      action: "moveTo",
+      keepTime: ANITIME.TOUCH_MOVE,
+      playTime: 0,
+      pos: pos
+    });
+    this.cmd.push({
+      action: "moveTo",
+      keepTime: ANITIME.TOUCH_MOVE,
+      playTime: ANITIME.TOUCH_MOVE,
+      pos: srcPos
+    });
+  }
 
-    moveTo(pos, playTime) {
-        var srcPos = cc.v2(this.x, this.y); 
-        this.cmd.push({
-            action: "moveTo",
-            keepTime: ANITIME.TOUCH_MOVE,
-            playTime: playTime,
-            pos: pos
-        });
-        this.x = pos.x;
-        this.y = pos.y;
-    }
+  moveTo(pos, playTime) {
+    var srcPos = cc.v2(this.x, this.y);
+    this.cmd.push({
+      action: "moveTo",
+      keepTime: ANITIME.TOUCH_MOVE,
+      playTime: playTime,
+      pos: pos
+    });
+    this.x = pos.x;
+    this.y = pos.y;
+  }
 
-    toDie(playTime) {
-        this.cmd.push({
-            action: "toDie",
-            playTime: playTime,
-            keepTime: ANITIME.DIE
-        });
-        this.isDeath = true;
-    }
+  toDie(playTime) {
+    this.cmd.push({
+      action: "toDie",
+      playTime: playTime,
+      keepTime: ANITIME.DIE
+    });
+    this.isDeath = true;
+  }
 
-    toShake(playTime) {
-        this.cmd.push({
-            action: "toShake",
-            playTime: playTime,
-            keepTime: ANITIME.DIE_SHAKE
-        });
-    }
+  toShake(playTime) {
+    this.cmd.push({
+      action: "toShake",
+      playTime: playTime,
+      keepTime: ANITIME.DIE_SHAKE
+    });
+  }
 
-    setVisible(playTime, isVisible) {
-        this.cmd.push({
-            action: "setVisible",
-            playTime: playTime,
-            keepTime: 0,
-            isVisible: isVisible
-        });
-    }
+  setVisible(playTime, isVisible) {
+    this.cmd.push({
+      action: "setVisible",
+      playTime: playTime,
+      keepTime: 0,
+      isVisible: isVisible
+    });
+  }
 
-    moveToAndDie(pos) {
+  moveToAndDie(pos) {
 
-    }
+  }
 
-    isBird() {
-        return this.type == CELL_TYPE.G;
-    }
+  isBird() {
+    return this.type == CELL_TYPE.G;
+  }
 
 }
